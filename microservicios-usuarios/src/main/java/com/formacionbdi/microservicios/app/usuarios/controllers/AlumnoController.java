ca76d0c7255bf0854fc8d78d14563faa2c7787f6
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-06-15 17:17:41 -0400

    Clase 30: Buscar alumnos usando operador like

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +29,11 @@ public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 		alumnoDB.setApellido(alumno.getApellido());
 		alumnoDB.setEmail(alumno.getEmail());
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDB));
+	}
+	
+	@GetMapping("/filtrar/{term}")
+	public ResponseEntity<?> filtrar(@PathVariable String term) {
+		return ResponseEntity.ok(service.findByNombreOrApellido(term));
 	}
 
 }
