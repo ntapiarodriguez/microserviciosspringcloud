5d47cb876b766049ad18dbf7ada4be36965fbdd3
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-06-15 20:09:04 -0400

    Sección 4: Backend Microservicio Cursos - 31. Usando join para obtener el curso asignado al alumno

--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/services/CursoServiceImpl.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/services/CursoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.microservicios.app.cursos.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.formacionbdi.microservicios.app.cursos.models.entity.Curso;
 import com.formacionbdi.microservicios.app.cursos.models.repository.CursoRepository;
@@ -8,5 +9,11 @@ import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
 
 @Service
 public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CursoService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public Curso findCursoByAlumnoId(Long id) {
+		return repository.findCursoByAlumnoId(id);
+	}
 
 }
