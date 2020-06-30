0e263007a0d2d7d3d043a76463e69c50ee17ed83
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-06-30 10:59:37 -0400

    40. Añadiendo asignaturas al examen

--- commons-examenes/src/main/java/com/formacionbdi/microservicios/commons/examenes/models/entity/Examen.java
+++ commons-examenes/src/main/java/com/formacionbdi/microservicios/commons/examenes/models/entity/Examen.java
@@ -11,6 +11,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
@@ -44,6 +45,9 @@ public class Examen {
 	@JsonIgnoreProperties(value = {"examen"}, allowSetters = true)
 	@OneToMany(mappedBy = "examen", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Pregunta> preguntas;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Asignatura asignatura;
 	
 	public Examen() {
 		this.preguntas = new ArrayList<>();
@@ -87,6 +91,14 @@ public class Examen {
 	public void removePregunta(Pregunta pregunta) {
 		this.preguntas.remove(pregunta);
 		pregunta.setExamen(null);
+	}
+
+	public Asignatura getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(Asignatura asignatura) {
+		this.asignatura = asignatura;
 	}
 
 	@Override
