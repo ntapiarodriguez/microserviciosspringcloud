644cb5934cfcd17a9c18a9fa9b85d6e90b5d6fdd
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-07-15 12:12:27 -0400

    77. Creando cliente HTTP Feign para comunicación con alumnos en microservicio cursos

--- microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/services/CursoServiceImpl.java
+++ microservicios-cursos/src/main/java/com/formacionbdi/microservicios/app/cursos/services/CursoServiceImpl.java
@@ -1,12 +1,16 @@
 package com.formacionbdi.microservicios.app.cursos.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.formacionbdi.microservicios.app.cursos.clients.AlumnoFeignClient;
 import com.formacionbdi.microservicios.app.cursos.clients.RespuestaFeignClient;
 import com.formacionbdi.microservicios.app.cursos.models.entity.Curso;
 import com.formacionbdi.microservicios.app.cursos.models.repository.CursoRepository;
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
 
 @Service
@@ -14,6 +18,9 @@ public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> 
 
 	@Autowired
 	RespuestaFeignClient client;
+	
+	@Autowired
+	AlumnoFeignClient clientAlumno;
 	
 	@Override
 	@Transactional(readOnly = true)
@@ -24,6 +31,11 @@ public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> 
 	@Override
 	public Iterable<Long> obtenerExamenesIdsConRespuestasAlumno(Long alumnoId) {
 		return client.obtenerExamenesIdsConRespuestasAlumno(alumnoId);
+	}
+
+	@Override
+	public Iterable<Alumno> obtenerAlumnosPorCurso(List<Long> ids) {
+		return clientAlumno.obtenerAlumnosPorCurso(ids);
 	}
 
 }
