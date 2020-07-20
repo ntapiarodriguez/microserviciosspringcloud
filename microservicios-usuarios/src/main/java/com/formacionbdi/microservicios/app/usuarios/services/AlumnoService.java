642080f9ae75d3612d749c0042b3b26d2d67b463
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-07-20 11:59:43 -0400

    82. Añadiendo cliente HTTP para comunicación con cursos en microservicio alumnos

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoService.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoService.java
@@ -10,5 +10,7 @@ public interface AlumnoService extends CommonService<Alumno>{
 	public List<Alumno> findByNombreOrApellido(String term);
 	
 	public Iterable<Alumno> findAllById(Iterable<Long> ids);
+	
+	public void eliminarCursoAlumnoPorId(Long id);
 
 }
