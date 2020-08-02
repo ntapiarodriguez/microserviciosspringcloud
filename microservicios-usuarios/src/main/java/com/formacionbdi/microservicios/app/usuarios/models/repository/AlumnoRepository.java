8531477b4c7e8bb29dc124f2546d40c603192517
Natalia Tapia <nataliatapia@MacBook-Pro-de-Natalia-2.local>
2020-08-02 15:06:46 -0400

    129. Backend: Añadiendo consulta JPA personalizada con OrderBy para ordenar por id

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -2,6 +2,8 @@ package com.formacionbdi.microservicios.app.usuarios.models.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -11,4 +13,8 @@ public interface AlumnoRepository extends PagingAndSortingRepository<Alumno, Lon
 
 	@Query("select a from Alumno a where upper(a.nombre) like upper(concat('%', ?1, '%')) or upper(a.apellido) like upper(concat('%', ?1, '%'))")
 	public List<Alumno> findByNombreOrApellido(String term);
+	
+	public Iterable<Alumno> findAllByOrderByIdAsc();
+	
+	public Page<Alumno> findAllByOrderByIdAsc(Pageable pageable);
 }
--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -3,6 +3,8 @@ package com.formacionbdi.microservicios.app.usuarios.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,19 +26,34 @@ public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepositor
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Iterable<Alumno> findAllById(Iterable<Long> ids) {
 		return repository.findAllById(ids);
 	}
-	
+
 	@Override
 	public void eliminarCursoAlumnoPorId(Long id) {
 		clientCurso.eliminarCursoAlumnoPorId(id);
 	}
-	
+
 	@Override
 	@Transactional
 	public void deleteById(Long id) {
 		super.deleteById(id);
 		this.eliminarCursoAlumnoPorId(id);
 	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Alumno> findAll() {
+		return repository.findAllByOrderByIdAsc();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Alumno> findAll(Pageable pageable) {
+		return repository.findAllByOrderByIdAsc(pageable);
+	}
+	
+
 }
